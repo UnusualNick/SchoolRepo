628abc88a60ba71475f4f242a4f2aaae65300301
UnusualNick <boboedov.i@yandex.ru>
2023-04-25 07:58:27 +0500

    github

---
+++ src/constants/statuses.js
@@ -0,0 +1,58 @@
+export const statuses = [
+  {
+    id: 1,
+    description: "В очереди",
+  },
+  {
+    id: 2,
+    description: "Обрабатывается",
+  },
+  {
+    id: 3,
+    description: "Принято",
+  },
+  {
+    id: 4,
+    description: "Неправильный ответ",
+  },
+  {
+    id: 5,
+    description: "Время вышло!",
+  },
+  {
+    id: 6,
+    description: "Ошибка компиляции",
+  },
+  {
+    id: 7,
+    description: "Ошибка выполнения (SIGSEGV)",
+  },
+  {
+    id: 8,
+    description: "Ошибка выполнения (SIGXFSZ)",
+  },
+  {
+    id: 9,
+    description: "Ошибка выполнения (SIGFPE)",
+  },
+  {
+    id: 10,
+    description: "Ошибка выполнения (SIGABRT)",
+  },
+  {
+    id: 11,
+    description: "Ошибка выполнения (NZEC)",
+  },
+  {
+    id: 12,
+    description: "Ошибка выполнения (Other)",
+  },
+  {
+    id: 13,
+    description: "Внутренняя ошибка",
+  },
+  {
+    id: 14,
+    description: "Exec Format Error",
+  },
+];
